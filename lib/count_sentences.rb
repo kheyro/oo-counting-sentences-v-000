86021d4ce3c43e234068e4e71997ea6acc64dc8f
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-07 22:21:37 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
   end
 
   def count_sentences
-    test = self.split(/[.!?]/).reject!(&:empty?).count()
+    test = self.split(/[.!?]/).reject!(&:empty?)
+    test.count()
   end
 end
